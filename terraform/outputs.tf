4ab98ab2cfba71be06073c652f9dae53a4e69b7f
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2024-11-17 12:24:52 -0800

    update output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,11 @@ output "s3_bucket_name" {
   value = aws_s3_bucket.web_bucket.bucket
 }
 
-output "command" {
-  value = "aws s3 sync ../web/ s3://${aws_s3_bucket.web_bucket.bucket}"
-}
+# output "command" {
+#   value = "aws s3 sync ../web/ s3://${aws_s3_bucket.web_bucket.bucket}"
+# }
 
 output "s3_website_url" {
   value = "http://${aws_s3_bucket.web_bucket.website_endpoint}"
+  depends_on = [ aws_s3_bucket_policy.web_bucket_policy ]
 }
