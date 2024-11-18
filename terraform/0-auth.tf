13ce23a038bab0ecdd1b1cd9a56f664eb6bd8fe3
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2024-11-17 16:41:26 -0800

    use line 1 of tf output, ignore debug

--- terraform/0-auth.tf
+++ terraform/0-auth.tf
@@ -6,12 +6,12 @@ terraform {
     }
   }
 
-  # backend "s3" {
-  #   bucket  = "tf-backend-4357232"              # Name of the S3 bucket
-  #   key     = "static-site-gh-actions.tfstate" # The name of the state file in the bucket
-  #   region  = "us-east-1"                       # Use a variable for the region
-  #   encrypt = true                              # Enable server-side encryption (optional but recommended)
-  # }
+  backend "s3" {
+    bucket  = "tf-backend-4357232"              # Name of the S3 bucket
+    key     = "static-site-gh-actions.tfstate" # The name of the state file in the bucket
+    region  = "us-east-1"                       # Use a variable for the region
+    encrypt = true                              # Enable server-side encryption (optional but recommended)
+  }
 }
 
 provider "aws" {
