eb0c4c1e74cc67ca3aa35fee51a7582caac557d6
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2024-11-17 16:28:25 -0800

    use line 1 of tf output, ignore debug

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,7 +6,7 @@ output "s3_bucket_name" {
 #   value = "aws s3 sync ../web/ s3://${aws_s3_bucket.web_bucket.bucket}"
 # }
 
-output "s3_website_url" {
-  value = "https://${aws_s3_bucket.web_bucket.website_endpoint}"
-  depends_on = [ aws_s3_bucket_policy.web_bucket_policy ]
-}
+# output "s3_website_url" {
+#   value = "https://${aws_s3_bucket.web_bucket.website_endpoint}"
+#   depends_on = [ aws_s3_bucket_policy.web_bucket_policy ]
+# }
